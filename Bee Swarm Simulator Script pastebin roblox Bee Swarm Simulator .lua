6281ddab82428b3427d00c82f2895b93e39d4566
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-26 07:20:13 +0100

    commit zero

--- Bee Swarm Simulator Script pastebin roblox Bee Swarm Simulator .lua
+++ Bee Swarm Simulator Script pastebin roblox Bee Swarm Simulator .lua
@@ -1,12 +1,11 @@
 -- find more powerfull verified and New Scripts here : https://script-pastebin.com
 
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/TSQ-new/Artem/master/Script')))() 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TSQ-new/Artem/master/TT"))();
 
 -- find more powerfull verified and New Scripts here : https://scriptpastebin.io
 
 --[[ Script Description:
 
-							functions :
-Auto - Dig  | Auto - Farm | Crash Server | Collect Jelly and more
+							A very good script for Bee Swarm Simulator that can crash the server and much more...
 						
 ]]
